1578297bd764ed1ad8bea5ab3d8e754cfdbfb5f6
Long Ho <holevietlong@gmail.com>
2011-09-11 15:22:11 -0400

    refactored jrpc.js to separate out private/public methods, added more exception checking, added echoArray to sample handler

--- handler.js
+++ handler.js
@@ -3,6 +3,9 @@ this.init = function () {
 		name : 'EchoHandler',
 		echo : function(str) {
 			return str;
+		},
+		echoArray : function(str1, str2) {
+			return str1 + "." + str2;
 		}
 	};
 }
--- jrpc.js
+++ jrpc.js
@@ -4,16 +4,7 @@ function extend(a, b) {
     }
 }
 var JRPCServer = {
-    
-    customPaths: { //Allow custom handlers for certain paths
-        '/version': function(url, res) {
-            JRPCServer.output('0.1.0', res);
-        },
-	'/modules': function(url, res) {
-	    JRPCServer.output(JRPCServer._modules, res);
-	}
-    },
-    
+	
     _errors: {
         'Parse Error': -32700,
         'Invalid Request': -32600,
@@ -24,11 +15,7 @@ var JRPCServer = {
     
     _modules: {},
     
-    registerModule: function(module) {
-        JRPCServer._modules[module.name] = module;
-    },
-    
-    generateError: function(reqId, errorName, errorMessage, errorData) {
+    _generateError: function(reqId, errorName, errorMessage, errorData) {
         var errors = JRPCServer._errors;
         if (!(errorName in errors)) {
             errorName = 'Internal Error';
@@ -44,6 +31,56 @@ var JRPCServer = {
         };
     },
     
+    _dispatch: function(jsonRequest) {
+        if (!jsonRequest) {
+	    return JRPCServer._generateError(null, 'Invalid Request', 'No request found');
+	}
+        if (!jsonRequest.hasOwnProperty('id')) {
+            return JRPCServer._generateError(null, 'Invalid Request', 'ID must be specified');
+        }
+        var reqId = jsonRequest.id;
+        if (!jsonRequest.hasOwnProperty('method')) {
+            return JRPCServer._generateError(reqId, 'Invalid Request', 'Method must be specified');
+        }
+
+	var methodArr = jsonRequest.method.split('\.');
+	var handler = JRPCServer._modules[methodArr[0]];
+	if (!handler || !handler.hasOwnProperty(methodArr[1])) {
+	    return JRPCServer._generateError(reqId, 'Method Not Found', jsonRequest.method);
+	}
+	var response;
+	if (Array.isArray(jsonRequest.params)) {
+	    try {
+		response = handler[methodArr[1]].apply(handler, jsonRequest.params);
+	    } catch (e) {
+		return JRPCServer._generateError(reqId, 'Internal Error', e);
+	    }
+	}
+	return {
+	   jsonrpc : '2.0',
+	   result : response,
+	   id : reqId
+	};
+    },
+    
+    _handleInvalidRequest : function(code, message, res) {
+        res.writeHead(code, {
+            'Content-Length': message.length,
+            'Content-Type': 'text/plain'
+        });
+        res.end(message);
+    },    
+
+    registerModule: function(module) {
+	JRPCServer._modules[module.name] = module;
+    },
+
+    customPaths: { //Allow custom handlers for certain paths
+        '/version': function(url, res) {
+            JRPCServer.output('0.1.0', res);
+        }
+    },
+    
     output: function(jsonResponse, res) {
         res.writeHead(200, {
             'Content-Type': 'application/json',
@@ -51,60 +88,39 @@ var JRPCServer = {
         });
         res.end(JSON.stringify(jsonResponse));
     },
-    
-    dispatch: function(jsonRequest) {
-	if (!jsonRequest) {
-	    return JRPCServer.generateError(null, 'Invalid Request', 'No request found');
-	}
-        if (!jsonRequest.hasOwnProperty('id')) {
-            return JRPCServer.generateError(null, 'Invalid Request', 'ID must be specified');
-        }
-        var reqId = jsonRequest.id;
-        if (!jsonRequest.hasOwnProperty('method')) {
-            return JRPCServer.generateError(reqId, 'Invalid Request', 'Method must be specified');
-        }
-	var methodArr = jsonRequest.method.split('\.');
-	var handler = JRPCServer._modules[methodArr[0]];
-	if (Array.isArray(jsonRequest.params)) {
-	   return handler[methodArr[1]].apply(handler, jsonRequest.params);
-	}
-        else 
-	   return {};
-    },
-    
+
     handle: function(req, res) {
         var jsonRequest = {};
         var url, response;
+        
         //Only accept GET & POST methods
         if (req.method != 'POST' && req.method != 'GET') {
-            error = 'Method can only be GET or POST';
-            res.writeHead(400, {
-                'Content-Length': error.length,
-                'Content-Type': 'text/plain'
-            });
-            res.end(error);
+            _handleInvalidRequest(400, 'Method can only be GET or POST', res);
             return;
         }
+        
         //Parse the URL
         try {
             url = require('url').parse(req.url, true);
         }
         catch (e) {
-            error = 'Malformed Request';
-            res.writeHead(400, {
-                'Content-Length': error.length,
-                'Content-Type': 'text/plain'
-            });
-            res.end(error);
+            _handleInvalidRequest(400, 'Malformed Request', res);
             return;
         }
+        
         //Allow certain paths to go thru
         if (url.pathname in JRPCServer.customPaths) {
-            JRPCServer.customPaths[url.pathname](url, res);
-            res.end();
+            try {
+                JRPCServer.customPaths[url.pathname](url, res);
+                res.end();
+            } catch (er) {
+                _handleInvalidRequest(400, 'Error resolving path' + url.pathname, res);
+            }
             return;
         }
+        
         if (req.method == 'POST') {
+            //Grab POST request
             jsonString = "";
             req.on('data', function(chunk) {
                 jsonString += chunk;
@@ -114,18 +130,20 @@ var JRPCServer = {
                     jsonRequest = JSON.parse(jsonString);
                 }
                 catch (err) {
-                    JRPCServer.output(JRPCServer.generateError(null, "Parse Error", err + ". Cannot parse message body: " + jsonString), res);
+                    JRPCServer.output(JRPCServer._generateError(null, "Parse Error", err + ". Cannot parse message body: " + jsonString), res);
                     return;
                 }
-                response = JRPCServer.dispatch(jsonRequest);
+                response = JRPCServer._dispatch(jsonRequest);
                 JRPCServer.output(response, res);
             });
         }
-        else if (req.method == 'GET') { //Allow GET method with param json=<json_string>
-            jsonRequest = url.query.json;
-            response = JRPCServer.dispatch(jsonRequest);
+        else if (req.method == 'GET') { //Allow GET method with params following JSON-RPC spec
+            jsonRequest = url.query;
+	    console.log(url.query);
+            response = JRPCServer._dispatch(jsonRequest);
             JRPCServer.output(response, res);
         }
     }
+    
 };
 extend(exports, JRPCServer);
